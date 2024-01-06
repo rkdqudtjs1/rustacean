10fd4e0a1010f5864430a5835c78622c8cd54990
byungseon <byungseon.kang@gmail.com>
2024-01-06 17:20:39 +0900

    variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -39,9 +39,52 @@ fn data_types() {
     // https://doc.rust-lang.org/stable/book/ch03-02-data-types.html#floating-point-types
 }
 
+fn numaric_operations() {
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated1 = -5 / 3; // Results in -1
+    let truncated2 = -5.0 / 3.0; // Results in -1
+
+    println!("quotient {quotient}");
+    println!("truncated1 {truncated1}");
+    println!("truncated2 {truncated2}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder {remainder}");
+}
+
+fn character() {
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+}
+
+fn tuple() {
+    let tup = (500, 'a', 1, 6.4);
+
+    let (a, b, c, d) = tup;
+    println!("a: {a} / b: {b} / c: {c} / d: {d}");
+
+    let one_of_tup = tup.0;
+    println!("one_of_tup(0) {one_of_tup}");
+}
+
+fn array() {
+    let a = [1, 2, 3, 4, 5];
+    let b = ['a', 'c', 'a'];
+    let c = ["aa", "cc", "aa"];
+
+    let d = ['a'; 5]; // ['a', 'a', 'a', 'a', 'a']
+
+    let d1 = d[1];
+    let d2 = d[2];
+}
+
 fn main() {
     mutable();
     constants();
     shadowing();
     data_types();
+    numaric_operations()
 }
